f261733f0a697da308f45428361a400c1eb431c7
VietNguyen <viet7896@gmail.com>
2023-09-27 10:31:46 +0700

    feature: User list,paging, search by name

--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -2,8 +2,13 @@ package com.example.ecommerce.service;
 
 import com.example.ecommerce.DTO.UserRegistrationDto;
 import com.example.ecommerce.model.User;
+import org.springframework.data.domain.Page;
+
 
 public interface UserService extends BaseService<Long, User> {
 
-    User save(UserRegistrationDto registrationDto );
+    User save(UserRegistrationDto registrationDto);
+
+    Page<User> searchByFullname(String name, int page, int size, String sortField, String sortOrder);
+
 }
